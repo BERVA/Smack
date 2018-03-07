299248950be4a31a708eeb97e97b053a366a0bd7
Barış Cesim <berva@Bars-MacBook-Pro.local>
2018-03-07 04:23:24 +0300

    lesson-04

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Barış Cesim on 7.03.2018.
+//  Copyright © 2018 bcesim. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Barış Cesim on 7.03.2018.
+//  Copyright © 2018 bcesim. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
